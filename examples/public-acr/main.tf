ba68b673e730cdd7f6f0cc903035e96d13cfe711
Erick Floresca <160937182+erick-floresca-nttd@users.noreply.github.com>
2025-01-16 23:26:15 -0500

    upgrade tooling, terraform version constraint, and primitive resource_name version (#6)
    * update terraform version constraint
    
    * update terraform version constraints and upgrade primitive resource_name module version

--- examples/public-acr/main.tf
+++ examples/public-acr/main.tf
@@ -17,7 +17,7 @@ resource "random_integer" "priority" {
 
 module "resource_names" {
   source  = "terraform.registry.launch.nttdata.com/module_library/resource_name/launch"
-  version = "~> 1.0"
+  version = "~> 2.0"
 
   for_each = var.resource_names_map
 
